d23235b029d38849f8945538d7e485474da0087b
OlehKurochka <o.kuro4ka@gmail.com>
2025-12-06 17:54:13 +0200

    corrected task #2

--- homeworks/homeworks_#3.py
+++ homeworks/homeworks_#3.py
@@ -20,24 +20,22 @@ if action == "+, -, *, /":
 else:
     print("Invalid action!")
 
+# 2. Перемістити елемент у списку
 
-# # 2. Перемістити елемент у списку
-#
-# lst = [1]
-# if lst:
-#     last_element = lst.pop()
-#     lst.insert(0, last_element)
-#     print(lst)
-# else:
-#     print(lst)
-#
-# # 3. Розділити один список на два списки
-#
-# lst = [1, 2, 3, 4, 5]
-# half_list = ((len(lst) + 1) // 2)
-# first_list = lst[:half_list]
-# second_list = lst[half_list:]
-# new_lst = list()
-# new_lst.append(first_list)
-# new_lst.append(second_list)
-# print(new_lst)
+lst = []
+if len(lst) > 1:
+    last_element = lst.pop()
+    lst.insert(0, last_element)
+print(lst)
+
+
+# 3. Розділити один список на два списки
+
+lst = [1, 2, 3, 4, 5]
+half_list = (len(lst) + 1) // 2
+first_list = lst[:half_list]
+second_list = lst[half_list:]
+new_lst = list()
+new_lst.append(first_list)
+new_lst.append(second_list)
+print(new_lst)
