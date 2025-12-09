c5dc5ee76f518e5707dfae13645ce9b87c204ac7
OlehKurochka <o.kuro4ka@gmail.com>
2025-12-09 22:45:33 +0200

    completed task 2

--- homeworks/homeworks_#4.py
+++ homeworks/homeworks_#4.py
@@ -2,9 +2,18 @@
 
 lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 
-for num in lst:
-    if num == 0:
-        lst.remove(num)
-        lst.append(num)
+for number in lst:
+    if number == 0:
+        lst.remove(number)
+        lst.append(number)
 print(lst)
+
+# 2. Знайти суму елементів із парними індексами
+
+lst = [0, 1, 7, 2, 4, 8]
+if lst:
+    summa = sum(lst[::2] * lst[-1])
+else:
+    summa = 0
+print(summa)
 
