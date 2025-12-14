936ada4146b3336aca250fa17f3c4045cf90a328
OlehKurochka <o.kuro4ka@gmail.com>
2025-12-14 20:39:42 +0200

    completed tasks #2 and #3

--- homeworks/homeworks_#5.py
+++ homeworks/homeworks_#5.py
@@ -1,3 +1,5 @@
+#1. Ім'я змінної
+
 from string import punctuation
 from keyword import kwlist
 
@@ -18,5 +20,53 @@ for char in text:
 for char in text:
     if char.isalpha() and not char.islower():
         result = False
-
 print(result)
+
+#2. Модифікувати калькулятор
+
+while True:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    action = input("What action to take (+, -, *, /): ")
+    result = None
+
+    if action == "+":
+        result = num1 + num2
+    elif action == "-":
+        result = num1 - num2
+    elif action == "*":
+        result = num1 * num2
+    elif action == "/":
+        if num2 == 0:
+            print("You can't divide by zero!")
+        else:
+            result = num1 / num2
+    else:
+        print("Invalid action!")
+    if result is not None:
+        print("Result:", result)
+
+    command = input("Continue? (y/n): ")
+    if command != "y":
+        break
+
+#3. hashtag
+
+from string import punctuation
+text = input("Enter text: ")
+
+text = text.title()
+text = text.replace(" ", "")
+
+new_text = ""
+for char in text:
+    if char not in punctuation:
+        new_text += char
+
+hashtag = "#"
+for char in new_text:
+    hashtag += char
+if len(hashtag) > 140:
+        hashtag = hashtag[:140]
+
+print(hashtag)
