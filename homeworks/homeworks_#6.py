a479dd7b252a17add57ea30ba2d89f8b51da84da
OlehKurochka <o.kuro4ka@gmail.com>
2025-12-18 17:13:35 +0200

    completed task 2

--- homeworks/homeworks_#6.py
+++ homeworks/homeworks_#6.py
@@ -11,4 +11,20 @@
 #
 # print(result)
 
-# 2. Конвертер із числа в дату
+# # 2. Конвертер із числа в дату
+#
+# number = int(input("Enter a number: "))
+# if number <= 8640000:
+#     day, n1 = divmod(number, 86400)
+#     hour, n2 = divmod(n1, 3600)
+#     minute, second = divmod(n2, 60)
+#     if day == 1:
+#         prnt_day = "день"
+#     elif 2 <= day % 10 <= 4 and not 11 <= day % 100 <= 14:
+#         prnt_day = "дні"
+#     else:
+#         prnt_day = "днів"
+#     time = f"{str(hour).zfill(2)}:{str(minute).zfill(2)}:{str(second).zfill(2)}"
+#     print(f"{day} {prnt_day}, {time}")
+# else:
+#     print("The number must be up to 8640000")
