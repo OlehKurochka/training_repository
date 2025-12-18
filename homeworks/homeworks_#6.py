1d1f4efff0a5d714436c8c131900a70356c3cdf7
OlehKurochka <o.kuro4ka@gmail.com>
2025-12-18 13:32:14 +0200

    copleted task 1

---
+++ homeworks/homeworks_#6.py
@@ -0,0 +1,14 @@
+# # 1. Діапазон букв
+# from string import ascii_letters
+# letters = input("Enter a two letters: ")
+#
+# first_letter, last_letter = letters.split("-")
+# start = ascii_letters.index(first_letter)
+# end = ascii_letters.index(last_letter)
+# result = " "
+# for ind in range(start, end + 1):
+#     result += ascii_letters[ind]
+#
+# print(result)
+
+# 2. Конвертер із числа в дату
