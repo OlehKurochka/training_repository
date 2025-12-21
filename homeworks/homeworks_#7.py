cd2e9e1b85a1f371c9e6ab21c9bf371d6c56d407
OlehKurochka <o.kuro4ka@gmail.com>
2025-12-21 21:18:25 +0200

    completed task 3

--- homeworks/homeworks_#7.py
+++ homeworks/homeworks_#7.py
@@ -23,3 +23,16 @@
 # print('ОК')
 
 # 3. Пошук підрядка
+
+# def second_index(text, some_str):
+#     first_ind = text.find(some_str)
+#     second_ind = text.find(some_str, first_ind + 1)
+#     if second_ind == -1:
+#         return None
+#     return second_ind
+#
+# assert second_index("sims", "s") == 3, 'Test1'
+# assert second_index("find the river", "e") == 12, 'Test2'
+# assert second_index("hi", "h") is None, 'Test3'
+# assert second_index("Hello, hello", "lo") == 10, 'Test4'
+# print('ОК')
