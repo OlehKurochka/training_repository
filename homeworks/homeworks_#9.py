20422821cc171a48250f1638a84d403be83f8e37
OlehKurochka <o.kuro4ka@gmail.com>
2026-01-07 20:13:09 +0200

    added file homeworks_#9 and completed tasks 1 and 2

---
+++ homeworks/homeworks_#9.py
@@ -0,0 +1,16 @@
+#1. Визначити популярність певних слів у тексті
+def popular_words(text: str, list_words: list ) -> dict:
+    text_lst = text.lower().split()
+    number_of_words = {word: text_lst.count(word) for word in list_words}
+    return number_of_words
+print(popular_words("When I was One I had just begun When I was Two I was nearly new", ['i', 'was', 'three', 'near']))
+
+###############################################################################################################
+
+#2. Різниця між числами
+def difference(*args):
+    if len(args) == 0:
+        return 0
+    else:
+        return round(max(args) - min(args), 2)
+print(difference(10.2, -2.2, 0, 1.1, 0.5))
